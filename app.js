3fc0268f16fd95339825640d6bdc0d2d006f88d0
ManojBinwal <mail.manojbinwal@gmail.com>
2023-03-26 03:37:42 +0530

    add task, delete task functions added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const tasks = [];
+let tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
@@ -9,8 +9,54 @@ function renderList () {}
 
 function markTaskAsComplete (taskId) {}
 
-function deleteTask (taskId) {}
+function deleteTask (taskId) {
 
-function addTask (task) {}
+    const newTasks = tasks.filter(function (task){
+        return task.id !== taskId
+    })
 
-function showNotification(text) {}
+    tasks = newTasks;
+    renderList();
+    showNotification('Task Deleted Successfully');
+
+}
+
+function addTask (task) {
+    if(task){
+        tasks.push(task);
+        renderList();
+        showNotification('Task added Successfully');
+        return;
+    }
+
+    showNotification('task cannot be added');
+
+}
+
+function showNotification(text) {
+    alert(text);
+}
+
+function handleInputKeypress (e) {
+    if (e.key==='Enter') {
+        const text = e.target.value;
+
+        console.log('text',text);
+
+    if(!text){
+        showNotification('task text cannot be empty');
+        return;
+    }
+const task = {
+    text,
+    id: Date.now().toString(),
+    done:false
+}
+e.target.value = '';
+addTask(task);
+
+    }
+}
+
+
+addTaskInput.addEventListener('keyup',handleInputKeypress);
